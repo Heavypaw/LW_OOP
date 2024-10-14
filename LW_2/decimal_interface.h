ba25c8b092326a10319904e3c832344c2a529705
Heavypaw <144659396+Heavypaw@users.noreply.github.com>
2024-10-14 15:16:03 +0300

    Update decimal_interface.h

--- LW_2/decimal_interface.h
+++ LW_2/decimal_interface.h
@@ -2,28 +2,23 @@
 #define DECIMAL_H
 
 #include <iostream>
-#include <vector>
 #include <stdexcept>
 
 class Decimal {
 public:
- 
     Decimal();
-    Decimal(const std::string& number);
+    Decimal(unsigned long long value);
+    Decimal(const std::string& str);
     Decimal(const Decimal& other);
-    Decimal(Decimal&& other) noexcept;
-    ~Decimal() noexcept;
+    ~Decimal();
 
-  
     Decimal& operator=(const Decimal& other);
-    Decimal& operator=(Decimal&& other) noexcept;
 
     Decimal operator+(const Decimal& other) const;
     Decimal operator-(const Decimal& other) const;
     Decimal& operator+=(const Decimal& other);
     Decimal& operator-=(const Decimal& other);
 
-   
     bool operator==(const Decimal& other) const;
     bool operator!=(const Decimal& other) const;
     bool operator<(const Decimal& other) const;
@@ -31,15 +26,17 @@ public:
     bool operator<=(const Decimal& other) const;
     bool operator>=(const Decimal& other) const;
 
-    
     friend std::ostream& operator<<(std::ostream& os, const Decimal& decimal);
-
-    
-    std::string toString() const;
+    friend std::istream& operator>>(std::istream& is, Decimal& decimal);
 
 private:
-    std::vector<unsigned char> digits;
+    unsigned char* digits;  
+    size_t size;           
+
     void normalize();
+    void resize(size_t newSize);
+    void setValue(unsigned long long value);
+    void setValue(const std::string& str);
 };
 
 #endif // DECIMAL_H
